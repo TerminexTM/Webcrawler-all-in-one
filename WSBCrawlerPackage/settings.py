bbfffa5de127f41b89532a6c52842327fe47ce79
dooster <dustin2burns@yahoo.com>
2021-09-08 07:55:49 -0700

    heroku changes

--- WSBCrawlerPackage/settings.py
+++ WSBCrawlerPackage/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'WSBCrawlerPackage.urls'
